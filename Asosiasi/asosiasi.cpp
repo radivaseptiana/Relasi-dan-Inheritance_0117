a8b467b2e01a21fb0896c086f8666e41039bf32b
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-27 18:49:25 +0700

    Mendeklarasikan member

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -9,6 +9,26 @@ class pasien{ //Asosisasi
         vector<dokter*> daftar_dokter;
         pasien(string pNama): nama(pNama) {
             cout << "Pasien \"" << nama << "\" ada\n";
-            
         }
+        ~pasien() {
+            cout << "Pasien \"" << nama
+            << "\" tidak ada\n";
+        }
+            void tambahDokter(dokter*);
+            void cetakDokter();
+};
+
+class dokter{
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama): nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama
+        << "\" tidak ada\n";
+    }
+    
 }
