1f8649e93bf6e876ad62f4446e891441f12ae51b
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-27 18:55:45 +0700

    mendeklarasikan perulangan

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -30,5 +30,20 @@ class dokter{
         cout << "Dokter \"" << nama
         << "\" tidak ada\n";
     }
-    
+        void tambahPasien(pasien*);
+        void cetakPasien();
+};
+
+void pasien:: tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter){
+        cout << a->nama <<"\n";
+    }
+    cout << endl;
 }
