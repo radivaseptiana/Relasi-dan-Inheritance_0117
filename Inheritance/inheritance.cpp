0cfa3f00861e081475dea2dc7fcf2d26ea275928
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-27 20:05:14 +0700

    mendeklarasikan member variabel

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -18,28 +18,28 @@ public:
             return a + b;
         }
 
-    };
+};
 
-    class pelajar : public orang {
-        public:
-            string sekolah;
+class pelajar : public orang {
+    public:
+        string sekolah;
         
-            pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah){
-                cout << "pelajar dibuat\n" << endl;
-            }
-            ~pelajar() {
-                cout << "pelajar dihapus\n" << endl;
-            }
-            string perkenalan() {
-                return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
-            }
-            };
+        pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah){
+            cout << "pelajar dibuat\n" << endl;
+        }
+        ~pelajar() {
+            cout << "pelajar dihapus\n" << endl;
+        }
+        string perkenalan() {
+            return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+        }
+};
         
-            int main() {
-                pelajar siswa1("andi laksono", "SMAN 1 Bantul");
-                cout << siswa1.perkenalan();
-                cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
         
-                return 0;
-            }
+    return 0;
+}
         
